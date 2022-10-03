177e441c860fe8bf9d226617f92dd30860422ce0
nicole-drml <doromal.cole@gmail.com>
2022-10-03 12:38:53 +0800

    sign in logic

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,38 @@
 import "./Login.scss";
 import Header from "../parts/Header";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Login = ({ data }) => {
   const navigate = useNavigate();
-  const location = useLocation();
-  const loginCreds = localStorage.getItem(data)
+  // const location = useLocation();
+  const savedData = localStorage.getItem(data)
     ? JSON.parse(localStorage.getItem(data))
     : [];
+
+  const [detailsValid, setDetailsValid] = useState(false);
 
   const [details, setDetails] = useState({
     username: "",
     password: "",
   });
 
+  // useEffect(() => {
+  //   detailsValid ? navigate("/");
+  //   console.log("is it valid?", detailsValid);
+  // }, [detailsValid]);
+// console.log("saved data", savedData)
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    {console.log('enlozi')}
-    console.log(details)
-    // data(details)
+    setDetailsValid(
+      savedData.some((data) => {
+        return (
+          data.username === details.username &&
+          data.password === details.password
+        );
+      })
+    );
   };
 
   // const [username, setUsername] = useState("")
@@ -46,13 +58,13 @@ const Login = ({ data }) => {
   //     : navigate("/admin/dashboard");
   // };
 
-  
   // onSubmit={handleSubmit}
   return (
     <>
       <Header />
       <div className="login">
         <form onSubmit={handleSubmit} className="login-form-container">
+      {!detailsValid && <span>invalid</span>}
           <h2>Hello!</h2>
           <div className="input-container">
             <label>Username:</label>
@@ -69,7 +81,7 @@ const Login = ({ data }) => {
             <label>Password:</label>
             <input
               type="password"
-              // value={password}
+              value={details.password}
               onChange={(e) =>
                 setDetails({ ...details, password: e.target.value })
               }
