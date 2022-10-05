278a83c54cf0b4814a7c8c11f8e9b74ec3a715c7
nicole-drml <doromal.cole@gmail.com>
2022-10-05 17:05:49 +0800

    finished dashboard graphs

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -9,16 +9,47 @@ const Dashboard = () => {
   const USERS = JSON.parse(localStorage.getItem("USERS"));
   const savedUsers = [...USERS];
 
+  const TRANSACTIONS = JSON.parse(localStorage.getItem("TRANSACTIONS"));
+  const transactionsHistory = [...TRANSACTIONS];
+
   const activeUsersData = [["Client", "Balance"]];
+
   let activeUsersBalance = 0;
   let pendingRequests = 0;
   const vipClients = [];
 
+  const activeUsersDataOptions = {
+    legend: "none",
+    pieSliceText: "none",
+    pieHole: 0.5,
+    is3D: false,
+    backgroundColor: "#e3e3e3",
+  };
+
+  let vipShares = 0;
+  let notVipShares = 0;
+
+  const vipSharesGraphOptions = {
+    legend: "none",
+    pieSliceText: "VIP",
+    pieStartAngle: 135,
+    tooltip: { trigger: "none" },
+    slices: {
+      0: { color: "brown" },
+      1: { color: "white" },
+    },
+    backgroundColor: "transparent",
+    allowHtml: true,
+    cssClassNames: {},
+  };
+
   savedUsers.forEach((user) => {
     const { ...users } = user;
     const { firstName, lastName, balance, status } = users;
+    const fullName = `${firstName} ${lastName}`;
+
     if (status === "ACTIVE") {
-      const activeUserArray = [lastName, parseFloat(balance)];
+      const activeUserArray = [fullName, parseFloat(balance)];
       activeUsersData.push(activeUserArray);
       activeUsersBalance += parseFloat(balance);
     }
@@ -26,31 +57,73 @@ const Dashboard = () => {
     if (status === "PENDING") {
       pendingRequests += 1;
     }
+
+    if (parseFloat(balance) >= 1000000) {
+      vipShares += parseFloat(balance);
+    } else {
+      notVipShares += parseFloat(balance);
+    }
   });
 
-  console.log("dataaa", activeUsersData);
+  let vipSharesPercentage = (vipShares / (vipShares + notVipShares)) * 100;
+  let notVipSharesPercentage =
+    (notVipShares / (vipShares + notVipShares)) * 100;
 
-  const options = {
-    pieHole: 0.5,
-    is3D: false,
-    backgroundColor: "#e3e3e3",
-  };
+  const vipSharesData = [
+    ["VIP Shares", "Percentage"],
+    ["VIP", vipSharesPercentage],
+    ["Not VIP", notVipSharesPercentage],
+  ];
 
   return (
     <div className="dashboard">
       <div className="dashboard-graph active-users-graph">
         <h3>Active Users</h3>
-        <Chart chartType="PieChart" data={activeUsersData} options={options} />
+        <div className="active-users-pie-chart">
+          <Chart
+            chartType="PieChart"
+            data={activeUsersData}
+            options={activeUsersDataOptions}
+          />
+        </div>
         <p className="active-users-text">
-          <span>{activeUsersData.length - 1}</span> active users with <br />
-          <span>₱{activeUsersBalance}</span> total balance.
+          <span>{activeUsersData.length - 1}</span> active users with
+          <span> ₱{activeUsersBalance}</span> total balance.
         </p>
       </div>
-      <div className="dashboard-graph graph-2">
-        <h3>Graph 2</h3>
+      <div className="dashboard-graph vip-shares-graph">
+        <h3>VIP Shares</h3>
+        <div className="vip-shares-pie-chart">
+          <Chart
+            chartType="PieChart"
+            data={vipSharesData}
+            options={vipSharesGraphOptions}
+          />
+        </div>
       </div>
-      <div className="dashboard-graph graph-3">
-        <h3>Graph 3</h3>
+      <div className="dashboard-graph pending-graph">
+        <h3>Pending</h3>
+        {pendingRequests > 0 && (
+          <div>
+            <span className="pending-number">{pendingRequests}</span>
+            <br></br>
+            <span className="activation-text">
+              activation {pendingRequests === 1 ? "request" : "requests"}
+            </span>{" "}
+          </div>
+        )}{" "}
+        {pendingRequests === 0 && (
+          <p className="no-requests-text">No activation requests.</p>
+        )}
+      </div>
+      <div className="dashboard-graph transactions-graph">
+        <h3>TRANSACTIONS</h3>
+        <div className="transactions-text-container">
+          <span className="transactions-number">{transactionsHistory.length}</span><br></br>
+            <span className="transactions-span">
+              total successful transactions.
+            </span>{" "}
+        </div>
       </div>
       <div className="dashboard-graph vip-graph">
         <h3>
@@ -64,26 +137,13 @@ const Dashboard = () => {
               return (
                 <li key={idx}>
                   {" "}
-                  {lastName}, {firstName} 
+                  {lastName}, {firstName}
                 </li>
               );
             }
           })}
         </ul>
         <p> {vipClients}</p>
-      </div>
-      <div className="dashboard-graph pending-graph">
-        <h3>Pending</h3>
-        {pendingRequests > 0 && (
-          <div>
-            <span className="pending-number">{pendingRequests}</span>
-            <br></br>
-            <span className="activation-text">
-              activation {pendingRequests === 1 ? "request" : "requests"}
-            </span>{" "}
-          </div>
-        )}{" "}
-        {pendingRequests === 0 && <p className="no-requests-text">No activation requests.</p>}
       </div>
     </div>
   );
